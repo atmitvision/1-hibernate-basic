a0affe820347bc64a889f8ade75181a32f2b16f5
Ashok Kumar Chowdhury <ashokchy1030@gmail.com>
2024-09-23 11:20:37 +0600

    Spring Data Rest commit

--- src/main/java/com/atmitvision/restapicruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/atmitvision/restapicruddemo/dao/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.atmitvision.restapicruddemo.dao;
 import com.atmitvision.restapicruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
